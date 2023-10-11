34195f980a847764ff0a35aab139f019d554487b
Xi Huang <Otomadmt@users.noreply.github.com>
2023-10-11 07:28:59 +0000

    Proceed to options2

--- exercises/vecs/vecs1.rs
+++ exercises/vecs/vecs1.rs
@@ -10,8 +10,7 @@
 
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // a plain array
-    let v = vec![10, 20, 30, 40];
-    // Note: Alternatively, use `let v = a.to_vec();`
+    let v = vec![10, 20, 30, 40]; // Note: Alternatively, use `let v = a.to_vec();`
 
     (a, v)
 }
