e0ee142c16af4c3b4dfe5f863e3349932c954262
Xi Huang <Otomadmt@users.noreply.github.com>
2023-10-15 17:32:06 +0000

    Proceed to using_as

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -5,8 +5,10 @@
 // Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
+// PONDER: what is `#[macro_use] extern crate` and `#[macro_export]`?
+// READ: https://veykril.github.io/tlborm/
+// READ: https://doc.rust-lang.org/1.29.0/book/first-edition/macros.html?highlight=macro_use#scoping-and-macro-importexport
+#[macro_use]
 mod macros {
     macro_rules! my_macro {
         () => {
